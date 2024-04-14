bea0473dacda6bc00b1794d3ecbbdfb34b19ae75
Романов Артем <romanov.artyom@gmail.com>
2024-04-14 12:25:56 +0000

    terraform-2\add new packer builds

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.94.0"
+      version = "0.93.0"
     }
   }
   required_version = ">= 0.13"
