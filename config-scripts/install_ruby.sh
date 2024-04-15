221d6f66d9405b972435ea6aa62137e43a545ee7
Романов Артем <romanov.artyom@gmail.com>
2024-04-15 13:33:52 +0000

    ansible-1\fix1

--- config-scripts/install_ruby.sh
+++ config-scripts/install_ruby.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sleep 1m
-while sudo sudo apt install -y ruby-full ruby-bundler build-essential; [ $? -ne 0 ]; do
+while sudo sudo apt install -y git ruby-full ruby-bundler build-essential; [ $? -ne 0 ]; do
     echo "Waiting for dpkg lock to be released, then retrying..."
     sleep 30s
 done
--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,4 +1,12 @@
-
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.94.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
 provider "yandex" {
   token     = var.token
   cloud_id  = var.cloud_id
