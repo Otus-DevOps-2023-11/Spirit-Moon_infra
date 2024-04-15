ad97fa90669cacf0b79b154d732f565403983a69
Романов Артем <romanov.artyom@gmail.com>
2024-04-15 14:25:26 +0000

    ansible-1\fix4

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,12 +1,20 @@
-#
-#provider "yandex" {
-#  token     = var.token
-#  cloud_id  = var.cloud_id
-#  folder_id = var.folder_id
-#  zone      = var.zone
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source  = "yandex-cloud/yandex"
+#      version = "0.94.0"
+#    }
+#  }
+#  required_version = ">= 0.13"
 #}
-#module "app" {
-#  source          = "../modules/app"
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
+module "app" {
+  source          = "../modules/app"
   public_key_path = var.public_key_path
   app_disk_image  = var.app_disk_image
   subnet_id       = var.subnet_id
